961924507f5258a4000c8066b77bd8621b572728
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-11-29 11:10:44 +0100

    main

---
+++ main.c
@@ -0,0 +1,10 @@
+/*
+ * File: main.c
+ * Authors: Myriam BELKHIRI
+ *			Aymeric LE PEN-RAVOUX 
+ *			Natnael DEBOCH
+ */
+
+#include "main.h"
+
+
---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/*
+ * File: main.h
+ * Authors: Myriam BELKHIRI
+ *			Aymeric LE PEN-RAVOUX
+ *			Natnael DEBOCH
+ */
+
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -
+#define EXIT -
+
+/*
