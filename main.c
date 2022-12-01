bccd26bd60e7d366903642c4945a5252fe4bbdf2
AymericLPR <mric31400@gmail.com>
2022-12-01 03:14:55 -0800

    function to open shell

--- main.c
+++ main.c
@@ -1,10 +1,56 @@
-/*
- * File: main.c
- * Authors: Myriam BELKHIRI
- *			Aymeric LE PEN-RAVOUX 
- *			Natnael DEBOCH
- */
-
 #include "main.h"
 
+/**
+ * main - access to simple shell
+ * Return: Always 0.
+ */
 
+int main(void)
+{
+  char *line;
+  pid_t pid = fork();
+  size_t len = 0;
+  size_t words;  
+
+  if (line == NULL)
+    {
+      perror("Code nonexistent");
+      return (-1);
+    }
+  if (pid == -1)
+    {
+      perror("Unsuccessful");
+      return (1);
+    }
+  if (pid == 0)
+    {
+      while (!(words == feof))
+	{
+	  printf("$ ");
+	  words = getline(&line, &len, stdin);
+	  if (/* pas "exit" */)
+	    {
+	      printf("%s\n", line);
+	    }
+	  else
+	    {
+	      free(line);
+	      line = NULL;
+	      exit(0);
+	    }
+	}
+      if (words == feof)
+	{
+	  free(line);
+	  line = NULL;
+	  exit(0);
+	}
+    }
+  else
+    {
+      wait(NULL);
+    }
+  free(line);
+  line = NULL;
+  return (0);
+}
