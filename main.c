83a08b9f0da809b974df78ae3ced4b6f35fc31cc
AymericLPR <mric31400@gmail.com>
2022-12-06 06:21:24 -0800

    function to open shell

--- main.c
+++ main.c
@@ -8,22 +8,23 @@
  * Return: Always 0.
  */
 
-int main(__attribute__((unused))int argc, __attribute__((unused))char **argv, char **envp)
+int main(__attribute__((unused))int argc, char **argv, char **envp)
 {
 char *line;
 size_t len = 0;
 char **cmd;
+(void)argv;
 
 line = (char *)malloc(len * sizeof(char));
 while (1)
 {
 getline(&line, &len, stdin);
-if (line == NULL)
-perror("Unsuccess");
 if (feof(stdin) || strcmp(line, "exit\n") == 0)
 {
 exit(0);
 }
+else if (strcmp(line, "env\n") == 0)
+penv(envp);
 else
 {
 cmd = get_cmd(line);
