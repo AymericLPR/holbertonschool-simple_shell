a050ee92edb11f77085ca466831628580717a56d
AymericLPR <mric31400@gmail.com>
2022-12-06 01:40:34 -0800

    function shell

--- main.c
+++ main.c
@@ -5,55 +5,32 @@
  * Return: Always 0.
  */
 
-int main(void)
+int main(int argc, char **argv, char **envp)
 {
   char *line;
   pid_t pid;
-  size_t len = 0;  
-  size_t words;
-  if (line == NULL)
-    {
-      perror("Code nonexistent");
-      return (-1);
-    }
-
-  pid = fork();
+  size_t len = 0;
+  char **cmd;
   
-  if (pid == -1)
+  line = (char *)malloc(len * sizeof(char));
+  while (1)
     {
-      return (1);
-    }
-  if (pid == 0)
-    {
-      while (1)
+      printf("$ ");
+      getline(&line, &len, stdin);
+      
+      if (line == NULL)
+	perror("Unsuccess");
+      if (feof(stdin) || strcmp(line, "exit\n") == 0)
 	{
-	  printf("$ ");
-	  getline(&line, &len, stdin);
-	  if (line[0] == 'e' && line[1] == 'x' && line[2] == 'i' && line[3] == 't')
-	    {
-	      free(line);
-	      line = NULL;
-	      printf("exit\n");
-	      exit(0);
-	    }
-	  else if (feof(stdin))
-	    {
-	      free(line);
-	      line = NULL;
-	      printf("exit\n");
-	      exit(0);
-	    }
-	  else
-	    {
-	      printf("%s\n",line);
-	    }
+	  printf("Exit successful\n");
+	  exit(0);
+	}
+      else
+	{
+	  cmd = get_cmd(line);
+	  frk(cmd, envp, argv[0]);
 	}
     }
-  else
-    {
-      wait(NULL);
-    }
   free(line);
-  line = NULL;
   return (0);
 }
