7fb3536513b1433aad400edcb1b56a6129d8abcf
AymericLPR <mric31400@gmail.com>
2022-12-06 02:04:28 -0800

    function to open shell

--- main.c
+++ main.c
@@ -10,30 +10,29 @@
 
 int main(int argc, char **argv, char **envp)
 {
-  char *line;
-  pid_t pid;
-  size_t len = 0;
-  char **cmd;
-  
-  line = (char *)malloc(len * sizeof(char));
-  while (1)
-    {
-      printf("$ ");
-      getline(&line, &len, stdin);
-      
-      if (line == NULL)
-	      perror("Unsuccess");
-      if (feof(stdin) || strcmp(line, "exit\n") == 0)
-	{
-	  printf("Exit successful\n");
-	  exit(0);
-	}
-      else
-	{
-	  cmd = get_cmd(line);
-	  frk(cmd, envp, argv[0]);
-	}
-    }
-  free(line);
-  return (0);
+char *line;
+pid_t pid;
+size_t len = 0;
+char **cmd;
+
+line = (char *)malloc(len * sizeof(char));
+while (1)
+{
+printf("$ ");
+getline(&line, &len, stdin);
+if (line == NULL)
+perror("Unsuccess");
+if (feof(stdin) || strcmp(line, "exit\n") == 0)
+{
+printf("Exit successful\n");
+exit(0);
+}
+else
+{
+cmd = get_cmd(line);
+frk(cmd, envp, argv[0]);
+}
+}
+free(line);
+return (0);
 }
