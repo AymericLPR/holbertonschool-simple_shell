015bb13ba9cb66762333be7ea9e50f44cdd5e6c8
AymericLPR <mric31400@gmail.com>
2022-12-06 01:41:43 -0800

    function fork

---
+++ fork.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * frk - function fork
+ * @cmd: command
+ * @envp: environement variable
+ * @filename: shell
+ */
+
+void frk(char **cmd, char **envp, char *filename)
+{
+  pid_t pid;
+  
+  pid = fork();
+
+      if (pid == 0)
+	{
+	  if (execve(cmd[0], cmd, envp) == -1)
+	    perror("Code nonexistent");
+	  exit(0);
+	}
+      else
+        wait(NULL);
+}
