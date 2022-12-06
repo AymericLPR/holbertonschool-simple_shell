1f1424cf1bfaefbf219f971ba6e693801e19074e
AymericLPR <mric31400@gmail.com>
2022-12-06 02:13:35 -0800

    function to take the command

--- get_command.c
+++ get_command.c
@@ -2,26 +2,27 @@
 
 /**
  * get_cmd - get the command
+ * @line: the command's user
  * Return: Always 0.
  */
 
 char **get_cmd(char *line)
 {
-	char *tok;
-	char delim[] = " \n\t";
-	char **cmd;
-	int i = 0;
+char *tok;
+char delim[] = " \n\t";
+char **cmd;
+int i = 0;
 
-	cmd = malloc(sizeof(char *) * 1024);
-	tok = strtok(line, delim);
+cmd = malloc(sizeof(char *) * 1024);
+tok = strtok(line, delim);
 
-	while (tok)
-	{
-		cmd[i] = tok;
-		tok = strtok(NULL, line);
-		i++;
-	}
-	if (cmd == NULL)
-	  printf("Error");
-	return (cmd);
+while (tok)
+{
+cmd[i] = tok;
+tok = strtok(NULL, line);
+i++;
+}
+if (cmd == NULL)
+printf("Error");
+return (cmd);
 }
