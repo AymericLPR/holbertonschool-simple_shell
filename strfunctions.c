a9c52db090c73363c9c8352d7f17f5e3436f10c0
MyriamHolberton <meriem_belkhiri@hotmail.com>
2022-12-07 12:21:46 +0100

    str

--- strfunctions.c
+++ strfunctions.c
@@ -58,4 +58,49 @@ if (s1 == NULL)
 len1 = 0;
 
 else
-len1 = _strlen(s2);
+len1 = _strlen(s1);
+
+if (s2 == NULL)
+len2 = 0;
+
+else
+len2 = _strlen(s2);
+
+str = malloc((len1 + len2 + 2) * sizeof(char));
+if (str == NULL)
+return (NULL);
+
+for (i = 0; i < len1; i++)
+str[i] = s1[i];
+str[i] = '/';
+i++;
+
+for (n = 0, n < len2, n++, i++)
+str[i] = s2[n];
+str[i] = '\0';
+free(s1);
+return (str);
+}
+/**
+ * _stcmp - compares strings s1 and s2
+ * @s1: first string to be compared
+ * @s2: second string being compared with the first
+ *
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int r = 0;
+
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+{
+
+if (r == 0)
+r = s1[i] - s2[i];
+
+else
+break;
+}
+return (r);
+}
